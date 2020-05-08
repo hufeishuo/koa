dff50b9c0e2e0bc693c9644ff6a9a6c840297a92
hufei <hufeicom@qq.com>
2020-05-08 18:25:19 +0800

    first read

---
+++ demo.js
@@ -0,0 +1,39 @@
+const Koa = require('./')
+const Router = require('koa-router')
+
+const router = new Router()
+
+router.get('/abc', (ctx, next)=>{
+    console.log( 'get /abc')
+    ctx.body = " get /abc"
+    next()
+}).get('/', (ctx, next)=>{
+    console.log( 'get / ')
+    next()
+})
+
+const app = new Koa()
+
+app.use( router.routes())
+
+
+
+console.log( app.toJSON() )
+
+app.use((ctx, next)=>{
+    ctx.res.end( 'hello world\ngoogd')
+})
+
+console.log( Reflect.ownKeys( app) )
+
+// app.listen实际上可以看做语法糖
+// 
+app.listen( 8999 )
+
+// 手动创建一个http 服务
+const http = require('http')
+
+http.createServer((req, res)=>{
+
+}).listen( 8998 )
+
---
+++ notebook.md
@@ -0,0 +1,36 @@
+Koa 实例上有哪些方法？？
+
+![koa app](./imgs/Koa-app.png)
+
+
+## Koa程序流程
+
+
+http.createServer( app.callback()).listen()
+
+app.callback() -> handleRequest() -> createContext 
+
+onerror
+
+
+1. http.createServer创建一个http服务进程，这个方法接收一个函数作为有http请求时回调；
+2. app.callback 组合middleware并返回了一个函数，这个函数处理http 请求；
+3. 当有http请求时，调用createContext方法创建上下文ctx;
+4. 将创建的上下文交给组合后的middleware处理；
+
+
+## Koa洋葱模型解释
+
+
+理解了compose方法就理解了洋葱模型
+
+
+### Koa洋葱模型解决的问题
+
+1. callback 导致的回调地域，便于已同步的方式写异步程序；
+2. 为程序扩展提供了便捷的接口；
+
+
+
+## delegate Node原生 request/response
+
